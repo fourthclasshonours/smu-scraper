c767caa24986475395d6411b28d9af40576d1347
Amos <alphatrl1997@gmail.com>
2021-07-14 01:46:12 +0800

    refactor: new school term format

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,6 +1,4 @@
-import { default as community } from './community';
-import { default as guides } from './guides';
 import { default as importantDates } from './important_dates';
 import { default as schoolTerms } from './sch_terms';
 
-export default [importantDates, schoolTerms, community, guides];
+export default [importantDates, schoolTerms];
